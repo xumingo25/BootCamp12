937a67db79c8994fe6e793b0f6e32ace7783161b
Poipurin <b.pinorojo@gmail.com>
2023-04-12 12:08:48 -0400

    se añade primer test/uso de selenium

---
+++ app/src/test/java/demoSelenium/bpino/BrowserTest.java
@@ -0,0 +1,46 @@
+package demoSelenium.bpino;
+
+//import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+class BrowserTest {
+
+    WebDriver driver;
+
+    @BeforeAll
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setupTest() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+
+    @Test
+    void test() {
+        // Exercise
+        driver.get("https://www.google.com/");
+        String title = driver.getTitle();
+        System.out.println("Título de página: "+title);
+
+        driver.navigate().to("https://www.youtube.com/");
+        title = driver.getTitle();
+        System.out.println("Título de página: "+title);
+        // Verify
+        //assertThat(title).contains("Selenium WebDriver");
+    }
+
+}
