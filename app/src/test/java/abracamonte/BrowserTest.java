784f699600920e72b020bdcf1e41ccbec4cfc8e4
abrilonia <abrilbracamonte@gmail.com>
2023-04-13 10:58:18 -0400

    Cambios en package abracamonte

---
+++ app/src/test/java/abracamonte/BrowserTest.java
@@ -0,0 +1,49 @@
+package abracamonte;
+
+//import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+class ChromeTest {
+
+    WebDriver driver;
+
+    @BeforeAll
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setupTest() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+
+    @Test
+    void test() {
+        // Exercise
+        driver.get("https://www.google.com");
+        String title = driver.getTitle();
+        System.out.println("Este es el titulo de la pagina: " + title);
+
+        driver.navigate().to("https://bonigarcia.dev/webdrivermanager/");
+        title = driver.getTitle();
+        System.out.println("Este es el titulo de la pagina: " + title);
+
+
+        // Verify
+        //assertThat(title).contains("Selenium WebDriver");
+    }
+
+}
