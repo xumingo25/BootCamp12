9775ae053edd0a7845c352eb258f0371fbdf666c
Lucia Galbiatti <luciagalbiatti@gmail.com>
2023-04-13 11:24:11 -0300

    Se agregan cambios

---
+++ app/src/test/java/LGalbiatti/BrowserTest.java
@@ -0,0 +1,43 @@
+package LGalbiatti;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+class ChromeTest {
+
+    WebDriver driver;
+
+    @BeforeAll
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setupTest() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+
+    @Test
+    void test() {
+        // Exercise
+        driver.get("https://www.google.com/");
+        String title = driver.getTitle();
+
+        System.out.println(title);
+
+        // Verify
+        //assertThat(title).contains("Selenium WebDriver");
+    }
+
+}
