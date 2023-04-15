d93cdf0eda56c3c36c7065b5e8491a8773242a29
daniela.pites <pitesdaniela@gmail.com>
2023-04-15 17:47:52 -0300

    se actualiza BrowserTest

--- app/src/test/java/dpites/BrowserTest.java
+++ app/src/test/java/dpites/BrowserTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
