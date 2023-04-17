97048552fec79ec527af40c2996a27ef0e98d12c
abrilonia <abrilbracamonte@gmail.com>
2023-04-17 09:30:13 -0400

    Se agrega test para Spotify en package TareaLocators

--- app/src/test/java/abracamonte/TareaLocators.java
+++ app/src/test/java/abracamonte/TareaLocators.java
@@ -1,22 +1,21 @@
 package abracamonte;
 
-//import static org.assertj.core.api.Assertions.assertThat;
-
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-
-class ChromeTest {
-
+public class TareaLocators {
     WebDriver driver;
+
+    @FindBy(xpath = "//button[contains(text(), 'Registra')]")
+    WebElement btnRegistrarse;
 
     @BeforeAll
     static void setupClass() {
@@ -36,19 +35,26 @@ class ChromeTest {
     @Test
     void test() {
         // Exercise
-        driver.get("https://www.google.com");
+        PageFactory.initElements(driver, this);
+        driver.get("https://www.spotify.com");
         String title = driver.getTitle();
         System.out.println("Este es el titulo de la pagina: " + title);
 
-        driver.navigate().to("https://bonigarcia.dev/webdrivermanager/");
+        driver.manage().window().maximize();
+
+        btnRegistrarse.click();
+
+        // Esperar a que se cargue la página de resultados
+        try {
+            Thread.sleep(5000); // Esperar 5 segundos
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         title = driver.getTitle();
-        System.out.println("Este es el titulo de la pagina: " + title);
+        System.out.println("Titulo de pagina: "+ title);
 
 
 
-
-        // Verify
-        //assertThat(title).contains("Selenium WebDriver");
     }
-
 }
