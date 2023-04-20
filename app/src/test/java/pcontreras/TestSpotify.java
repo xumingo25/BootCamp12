92f1fd403d87339e3d480470a443ab4438868b7c
Paloma Andrea <pal.contrerasm@gmail.com>
2023-04-20 01:03:09 -0400

    Se agrega lo que se lleva de pruebaSpotify y se comentan los casos que aún no se logra resolve r

--- app/src/test/java/pcontreras/TestSpotify.java
+++ app/src/test/java/pcontreras/TestSpotify.java
@@ -2,6 +2,7 @@ package pcontreras;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -31,9 +32,25 @@ class TareaLocators {
     }
 
     @Test
-    void testEjemploSpotify(){
+    void testEjemploSpotify() throws InterruptedException {
         //Aqui crearemos el test
         btnRegistrase.click();
+        Thread.sleep(5000);
+        WebElement email = driver.findElement(By.xpath("//*[@id=\"email\"]"));
+            email.sendKeys("mailfalso@gfalso.com");
+        WebElement password = driver.findElement(By.xpath("//*[@id=\"password\"]"));
+            password.sendKeys("ContraFalsa_1234");
+        WebElement name = driver.findElement(By.xpath("//*[@id=\"displayname\"]"));
+            name.sendKeys("Erbmon Oslaf");
+        WebElement birthDate = driver.findElement(By.xpath("//*[@id=\"day\"]"));
+            birthDate.sendKeys("17");
+        WebElement birthYear = driver.findElement(By.xpath("//*[@id=\"year\"]"));
+            birthYear.sendKeys("1994");
+        WebElement birthMonth = driver.findElement(By.xpath("//*[@id=\"month\"]"));
+            birthMonth.sendKeys("Febrero");
+        //WebElement gender = driver.findElement(By.xpath("//*[@id=\"gender_option_female\"]")).sendKeys("Mujer");
+        //WebElement optBox = driver.findElement(By.xpath("//*[@id=\"marketing_check_optbox\"]"));
+            optBox.click();
     }
 
     @AfterEach
