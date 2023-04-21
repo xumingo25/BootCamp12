8da4d7ef7d088e10f3f658669270d828f8850d70
Poipurin <b.pinorojo@gmail.com>
2023-04-21 11:50:44 -0400

    se crea HomePage

---
+++ app/src/test/java/bpino/pom/pages/HomePage.java
@@ -0,0 +1,19 @@
+package bpino.pom.pages;
+
+import bpino.pom.base.SeleniumBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage extends SeleniumBase {
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
+
+    By locatorRegistrarte = By.xpath("//button[contains(text(),'Registra')]");
+
+    public void irARegistrarte(){
+        if(estaDesplegado(locatorRegistrarte)){
+            clickear(locatorRegistrarte);
+        }
+    }
+}
