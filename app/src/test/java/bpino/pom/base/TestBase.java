6abd02c90d46bd558bbf422d05c7926220858f6a
Poipurin <b.pinorojo@gmail.com>
2023-04-21 12:00:44 -0400

    se crea TestBase

---
+++ app/src/test/java/bpino/pom/base/TestBase.java
@@ -0,0 +1,28 @@
+package bpino.pom.base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestBase {
+    public WebDriver driver;
+
+    @BeforeAll
+    public void initTesting(){
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    public void setUp(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+    }
+    @AfterEach
+    public void close(){
+        driver.close();
+    }
+}
