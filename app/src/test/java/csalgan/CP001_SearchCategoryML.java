27caeb9182563f053fd653fed272f63d67e1601f
Claudia Alexandra Salgan Bettuzzi <alexandrasalgan@gmail.com>
2023-04-21 11:13:43 -0300

    añadiendo tarea de mercado libre

---
+++ app/src/test/java/csalgan/CP001_SearchCategoryML.java
@@ -0,0 +1,21 @@
+package csalgan;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+public class CP001_SearchCategoryML {
+
+}
