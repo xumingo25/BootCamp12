5af3c35a04ef895b36c48135bf60fde89975df19
Poipurin <b.pinorojo@gmail.com>
2023-04-24 11:30:48 -0400

    se agrega MELI homepage

--- app/src/test/java/bpino/pom/pages/meli/HomePage.java
+++ app/src/test/java/bpino/pom/pages/meli/HomePage.java
@@ -1,4 +1,26 @@
 package bpino.pom.pages.meli;
 
-public class HomePage {
+import bpino.pom.base.SeleniumBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import static bpino.pom.utilities.ManejoEncodingUFT8.fixEncoding;
+
+public class HomePage extends SeleniumBase {
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
+    By locatorTxtCookies = By.xpath("//button[contains(text(),'Entendido')]");
+    By locatorTxtUbicacion = By.xpath(fixEncoding("//span[contains(text(),'Más tarde')]"));
+    By locatorInputProducto = By.xpath("//input[@id='cb1-edit']");
+    By locatorBtnBuscar = By.xpath("//button[@class= 'nav-search-btn']");
+
+    public void buscarProducto(String producto){
+        if(estaDesplegado(locatorTxtCookies)){
+          clickear(locatorTxtCookies);
+          clickear(locatorTxtUbicacion);
+          escribir(producto, locatorInputProducto);
+          clickear(locatorBtnBuscar);
+        }
+    }
 }
