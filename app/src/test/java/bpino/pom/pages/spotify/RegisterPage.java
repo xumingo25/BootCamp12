25111c8a8e54ea3497c8c06bd251ca753d5b0ab4
Poipurin <b.pinorojo@gmail.com>
2023-04-24 10:39:15 -0400

    se actualiza registerpage

--- app/src/test/java/bpino/pom/pages/spotify/RegisterPage.java
+++ app/src/test/java/bpino/pom/pages/spotify/RegisterPage.java
@@ -12,22 +12,20 @@ public class RegisterPage extends SeleniumBase {
         super(driver);
     }
 
-    By locatorTxtCorreo = By.xpath("");
-    By locatorTxtConfirmarCorreo = By.xpath("");
-    By locatorTxtContrasena = By.xpath("");
-    By locatorTxtApodo = By.xpath("");
-    By locatorTxtDia = By.xpath("");
-    By locatorCBDia = By.xpath("");
-    By locatorTxtConfirmarAnnio = By.xpath("");
-    By locatorCBAnnio = By.xpath("");
-    By locatorTxtMes = By.xpath("");
-    By locatorCBMes = By.xpath("");
-    By locatorGeneros = By.xpath("");
-    By locatorCheckBox = By.xpath("");
-    By locatorBtnRegistrarse = By.xpath("");
-    By locatorTxtErrorCorreoExistente = By.xpath("");
+    By locatorTxtCorreo = By.xpath("//input[@id='email']");
+    By locatorTxtConfirmarCorreo = By.xpath("//input[@name='confirm']");
+    By locatorTxtContrasena = By.xpath("//input[@type=\"password\"]");
+    By locatorTxtApodo = By.xpath("//input[@id='displayname']");
+    By locatorTxtDia = By.xpath("//input[@name='day']");
 
-    public void completarFormularioRegistro(String correo, String confirmarCorreo, String password, String apodo, String dia, String mes, String annio, String genero, boolean recibirMensaje, boolean recibirDatos) {
+    By locatorTxtAnnio = By.xpath("//input[@name='year']");
+    By locatorCBMes = By.xpath("//select");
+    By locatorGeneros = By.xpath("//label[contains(@for,'gender')]");
+    By locatorCheckBox = By.xpath("\"//label[contains(@for,'checkbox')]\"");
+    By locatorBtnRegistrarse = By.xpath("//button[@type=\"submit\"]");
+    By locatorTxtErrorCorreoExistente = By.xpath("//span[contains(text(),'Este correo')]");
+
+    public void completarFormularioRegistro(String correo, String confirmarCorreo, String password, String apodo, String dia, String mes, String annio, String genero, boolean recibirMensaje, boolean compartirDatos) {
         escribir(correo, locatorTxtCorreo);
         if (estaDesplegado(locatorTxtConfirmarCorreo)) {
             escribir(confirmarCorreo, locatorTxtConfirmarCorreo);
@@ -36,6 +34,8 @@ public class RegisterPage extends SeleniumBase {
         escribir(apodo, locatorTxtApodo);
         escribir(dia, locatorTxtDia);
         scroll(buscarElementoWeb(locatorTxtDia));
+        seleccionarCBPorTxtVisible(buscarElementoWeb(locatorCBMes), "Febrero");
+        escribir(annio, locatorTxtAnnio);
 
         List<WebElement> generos = buscarElementosWeb(locatorGeneros);
         switch (genero.toLowerCase()) {
@@ -43,17 +43,43 @@ public class RegisterPage extends SeleniumBase {
                 clickear(generos.get(0));
                 break;
             case "mujer":
+                clickear(generos.get(1));
                 break;
-            case "no binario":
+            case "no ninario":
+                clickear(generos.get(2));
                 break;
             case "otro":
+                clickear(generos.get(3));
                 break;
             case "prefiero no responder":
+                clickear(generos.get(4));
                 break;
             default:
+                clickear(generos.get(4));
                 break;
         }
+
+        List<WebElement> checks = buscarElementosWeb(locatorCheckBox);
+
+        scroll(checks.get(0));
+
+        if (recibirMensaje) {
+            clickear(checks.get(0));
+        }
+
+        scroll(checks.get(1));
+
+        if (compartirDatos) {
+            clickear(checks.get(1));
+        }
+
+        clickear(locatorBtnRegistrarse);
     }
 
+    public String obtenerErrorCorreoUtilizado(){
+        return obtenerTexto(locatorTxtErrorCorreoExistente);
+    }
 }
+
+
 
